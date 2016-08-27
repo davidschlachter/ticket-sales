df5f3eca2323d1acc2c48d22698cb76db8bd219f
David Schlachter <david+gh@schlachter.ca>
2016-08-27 13:31:20 -0400

    Add mongoose

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var mongoose = require('mongoose');
 var MongoStore = require('connect-mongo')(session);
 
 var routes = require('./routes/index');
 
 var app = express();
+
+var config = require('./config');
 
 // Connect to the database
 mongoose.connect(config.opt.db, config.opt.mongoose);
